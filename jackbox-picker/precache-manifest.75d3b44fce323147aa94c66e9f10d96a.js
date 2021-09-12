299825dc5a4c3c1f06f06f61cac32d08a0d2b88d
Nathan Freeman <natethegreat68.dev@gmail.com>
2021-09-12 15:56:04 -0400

    Added jackbox-picker.
    This was built from:
    https://github.com/NateTheGreat68/jackbox-picker/commit/57fac34d057da4e43113548cda4bc59a181a212b

---
+++ jackbox-picker/precache-manifest.75d3b44fce323147aa94c66e9f10d96a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed8a386f6d67ba67b7aedddff7fdfd7f",
+    "url": "/jackbox-picker/index.html"
+  },
+  {
+    "revision": "a993dd8dd754d32439b1",
+    "url": "/jackbox-picker/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "57d9451d0f09e5df5307",
+    "url": "/jackbox-picker/static/css/main.867ba02b.chunk.css"
+  },
+  {
+    "revision": "a993dd8dd754d32439b1",
+    "url": "/jackbox-picker/static/js/2.037ca67b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/jackbox-picker/static/js/2.037ca67b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57d9451d0f09e5df5307",
+    "url": "/jackbox-picker/static/js/main.13f4287a.chunk.js"
+  },
+  {
+    "revision": "12c6ece6bb5a63e19546",
+    "url": "/jackbox-picker/static/js/runtime-main.6a957121.js"
+  },
+  {
+    "revision": "2ad10486b54d7b09844e4d9815257f34",
+    "url": "/jackbox-picker/static/media/blather.2ad10486.webp"
+  },
+  {
+    "revision": "b00b91ff4f59beacc26bf93d1248275c",
+    "url": "/jackbox-picker/static/media/champd.b00b91ff.webp"
+  },
+  {
+    "revision": "340ae1f732283231ba34e46eed24f697",
+    "url": "/jackbox-picker/static/media/civic.340ae1f7.jpg"
+  },
+  {
+    "revision": "f18a6c44e3681c15ce9fb794f96cd50e",
+    "url": "/jackbox-picker/static/media/devils.f18a6c44.webp"
+  },
+  {
+    "revision": "3b85f4d106c6502c13821e3fc1af4a1d",
+    "url": "/jackbox-picker/static/media/drawful2.3b85f4d1.jpg"
+  },
+  {
+    "revision": "14dcba03cd82641f4b4b8a203c732aab",
+    "url": "/jackbox-picker/static/media/fibbage3.14dcba03.png"
+  },
+  {
+    "revision": "1418e13233461dc273c4230c9e6b160f",
+    "url": "/jackbox-picker/static/media/guess.1418e132.jpg"
+  },
+  {
+    "revision": "b81f888b0b81f9094847d91527aba403",
+    "url": "/jackbox-picker/static/media/joke.b81f888b.jpg"
+  },
+  {
+    "revision": "e3291d79b635b7fb8f15292890fab1a6",
+    "url": "/jackbox-picker/static/media/mad.e3291d79.jpg"
+  },
+  {
+    "revision": "dc65d347abf82ce8babda1afe78e2ef5",
+    "url": "/jackbox-picker/static/media/monster.dc65d347.jpg"
+  },
+  {
+    "revision": "1fbdaae957ad36acbdaa009ae30f8c1b",
+    "url": "/jackbox-picker/static/media/patently.1fbdaae9.png"
+  },
+  {
+    "revision": "767f89f968e3a385bec7892c905a2db9",
+    "url": "/jackbox-picker/static/media/quiplash3.767f89f9.webp"
+  },
+  {
+    "revision": "72dabfd71da493df8e2cf13b5c013218",
+    "url": "/jackbox-picker/static/media/supria-sans-heavy.72dabfd7.otf"
+  },
+  {
+    "revision": "f195ba96d8ff0aa7e28abef0afcfe9f7",
+    "url": "/jackbox-picker/static/media/talking.f195ba96.webp"
+  },
+  {
+    "revision": "ae47f7d6ddd51e6d7c2686087d8fe8c5",
+    "url": "/jackbox-picker/static/media/teeko.ae47f7d6.jpg"
+  },
+  {
+    "revision": "0c63bcd386dc94c825337a2070dcc049",
+    "url": "/jackbox-picker/static/media/ydkjfs.0c63bcd3.png"
+  }
+]);
